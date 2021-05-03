17753e386b26d3dddf34b5e55e5c8834ff7948b0
CyberPilot51 <77673498+CyberPilot51@users.noreply.github.com>
2021-05-03 19:01:10 +0200

    Add files via upload
    Code works in netbeans so please ensure netbeans is installed on your computer

---
+++ Data_Validation/src/data_validation/Data_Validation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+* Time spent: 4 days
+ */
+package data_validation;
+
+/**
+ *
+ * @author garet
+ */
+public class Data_Validation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
